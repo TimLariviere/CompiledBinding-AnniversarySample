c5435dffeb32a82032cff4d60289f209fd75588b
Timothé LARIVIERE <Timoth LARIVIERE>
2016-07-09 02:28:26 -0700

    Sample project for the new features with Compiled Binding for UWP

---
+++ CompiledBinding_AnniversaryEdition_Sample/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using Windows.UI.Xaml.Controls;
+
+namespace CompiledBinding_AnniversaryEdition_Sample
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        #region 01 - Function binding
+
+        public DateTime Today = DateTime.Now;
+
+        public Person Roberts = new Person()
+        {
+            Firstname = "Dread Pirate",
+            Lastname = "Roberts"
+        };
+
+        public string GetHelloWorld()
+        {
+            return "Hello World";
+        }
+
+        #endregion
+
+        #region 02 - Dictionary indexers
+
+        public Dictionary<string, string> Dictionary = new Dictionary<string, string>()
+        {
+            { "A", "Alpha" },
+            { "B", "Beta" },
+            { "C", "Charlie" },
+        };
+
+        #endregion
+
+        #region 03 - Explicit value cast
+
+        public bool? IsThreeStateNull = null;
+        public bool? IsThreeStateFalse = false;
+        public bool? IsThreeStateTrue = true;
+
+        #endregion
+
+        #region 04 - Implicit Visibility conversion
+
+        public bool IsVisibleFalse = false;
+        public bool IsVisibleTrue = true;
+
+        #endregion
+    }
+}
---
+++ CompiledBinding_AnniversaryEdition_Sample/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompiledBinding_AnniversaryEdition_Sample
+{
+    public class Person
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+
+        public string GetFullname()
+        {
+            return $"{Firstname} {Lastname}";
+        }
+    }
+}
